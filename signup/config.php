baf92f02b7b7e84332984c21884198d22ef53368
Mohammed Bakkali <94966939+mohammed-bakkali@users.noreply.github.com>
2023-02-22 09:25:55 +0100

    Add files via upload

---
+++ signup/config.php
@@ -0,0 +1,12 @@
+<!-- connection with databases -->
+
+<?php 
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'gestimmo';
+
+$connect = mysqli_connect($host,$user,$pass,$db); #commend connection with databases
+
+?>
