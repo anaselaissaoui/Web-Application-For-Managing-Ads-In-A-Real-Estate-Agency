7690795090b4d51c6555e7db9977db962afb9c0b
El Aissaoui Anas <elaissaoui.anas.solicode@gmail.com>
2023-02-25 23:54:31 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -9,20 +9,20 @@ CREATE TABLE client (
     id_client  INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     LName    VARCHAR(50),
     Name VARCHAR(20),
-    Email   VARCHAR(20),
-    Password     VARCHAR(20),
+    Email   VARCHAR(100),
+    Password     VARCHAR(100),
     PhoneN  VARCHAR(20)
     
 );
 
 CREATE TABLE  Annonce(
     id_annonce  INT NOT NULL  AUTO_INCREMENT,
-    Title VARCHAR(20),
+    Title VARCHAR(100),
     Price  DECIMAL,
     offerDate DATE,
     id_client INT,
     L_M_Date DATE,
-    Address VARCHAR(40),
+    Address VARCHAR(100),
     Category VARCHAR(20),
     offerType VARCHAR(20),
     PRIMARY KEY (id_annonce),
@@ -55,20 +55,7 @@ INSERT INTO annonce (Title, Surface,City,Address, Price, offerDate, L_M_Date,  C
   ('Spacious 4-Bedroom Villa with a Terrace', 250,'Las Vegas','666 Sunset Blvd', 700000, '2023-02-18','2023-02-18',  'Villa', 'Rental'),
   ('Modern 3-Bedroom Apartment with a View of the Park', 90,'Seattle','777 Forest Ave', 250000, '2023-02-19','2023-02-19',  'Apartment', 'Sale');
 
-  UPDATE annonce 
-SET Title = CASE Title 
-                     WHEN 'Modern 2-Bedroom Apa' THEN 'Modern 2-Bedroom Apartment in the Heart of the City'
-                     WHEN 'Spacious 3-Bedroom H' THEN 'Spacious 3-Bedroom House with a Private Garden'
-                     WHEN 'Luxurious 4-Bedroom ' THEN 'Luxurious 4-Bedroom Villa with a Swimming Pool'
-                     WHEN 'Stylish 1-Bedroom Ap' THEN 'Stylish 1-Bedroom Apartment in a Quiet Neighborhood'
-                     WHEN 'Cozy 2-Bedroom House' THEN 'Cozy 2-Bedroom House with a Fireplace'
-                     WHEN 'Charming 3-Bedroom V' THEN 'Charming 3-Bedroom Villa with Ocean Views'
-                     WHEN 'Renovated 2-Bedroom ' THEN 'Renovated 2-Bedroom Apartment with a Balcony'
-                     WHEN 'Quaint 1-Bedroom Hou' THEN 'Quaint 1-Bedroom House with a Garden'
-                     WHEN 'Spacious 4-Bedroom V' THEN 'Spacious 4-Bedroom Villa with a Terrace'
-                     WHEN 'Modern 3-Bedroom Apa' THEN 'Modern 3-Bedroom Apartment with a View of the Park'
-                     ELSE Title 
-                   END;
+ 
 
 
   INSERT INTO client (LName, Name, Email, Password, PhoneN) VALUES
